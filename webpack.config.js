d2585156621ab783bb3176826c70f580e6352444
Meathill <meathill@gmail.com>
2017-06-24 23:07:38 +0800

    架构

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,32 @@
 /**
  * Created by meathill on 2017/6/23.
  */
+
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'app'),
+  entry: {
+    preloader: './preloader.js',
+    main: './main.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devtool: 'source-map',
+  watch: true,
+  watchOptions: {
+    poll: 1000,
+    ignored: /node_modules|css|styl|dist|build|docs/
+  }
+};
